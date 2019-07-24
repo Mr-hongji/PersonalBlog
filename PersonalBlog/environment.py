28faedde7bc6c6796541731b4de082e7b50a97b0
shihongji <shihongji@xiaoneng.cn>
2019-07-24 17:56:28 +0800

    ‘修改读取的环境变量名

--- PersonalBlog/environment.py
+++ PersonalBlog/environment.py
@@ -4,7 +4,7 @@ import os
 
 class ENV(object):
     def __init__(self):
-        env = os.environ.get("oa_env", "local")
+        env = os.environ.get("perblog_env", "local")
         print('...............................')
         print(env)
         print('...............................')
