cc75eb106a2286afca582f9445d1b1fa02989922
shihongji <shihongji@xiaoneng.cn>
2019-07-31 15:30:03 +0800

    添加文档视频读取路径可根据线上线下环境自动加载

--- PersonalBlog/environment.py
+++ PersonalBlog/environment.py
@@ -1,6 +1,7 @@
 # __author__ = itsneo1990
 import os
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 class ENV(object):
     def __init__(self):
@@ -11,6 +12,16 @@ class ENV(object):
         file_name = env + "_settings"
         base_settings = __import__("PersonalBlog", fromlist=[file_name])
         self.settings = getattr(base_settings, file_name)
+
+        settingsJsPath = os.path.join(BASE_DIR, 'static/js/settings.js')
+        # print(settingsJsPath)
+
+        f = open(settingsJsPath, 'w')
+        if env == 'local':
+            f.write('var doc_root_path="e:/";\nvar video_root_path="h:/";')
+        else:
+            f.write('var doc_root_path="/root/files/docfile";\nvar video_root_path="/root/files/videofile";')
+        f.close()
 
     def get_config(self, value):
         return getattr(self.settings, value, None)
--- static/js/settings.js
+++ static/js/settings.js
@@ -1,4 +1,2 @@
-// var doc_root_path="/opt/files/docfile/";
-// var video_root_path="/opt/files/videofile/";
 var doc_root_path="e:/";
 var video_root_path="h:/";
