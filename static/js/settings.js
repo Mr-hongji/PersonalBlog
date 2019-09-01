5c90e7fd3c73cb7818dab0b8eb88390407d87a45
shihongji <lyjn5126@163.com>
2019-09-01 22:54:41 +0800

    修改文章详情中的图片大小自适应

--- static/js/settings.js
+++ static/js/settings.js
@@ -1,3 +1,3 @@
-var doc_root_path="/opt/files/docfile/";
-var video_root_path="/opt/files/videofile/";
-var uploadImageRootLoaction = "http://perblog.natapp1.cc/articleImg/";
+var doc_root_path="e:/";
+var video_root_path="h:/";
+var uploadImageRootLoaction = "http://127.0.0.1:8000/static/uploadfiles/articleImages/";
